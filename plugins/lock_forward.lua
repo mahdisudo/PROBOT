210a90fb507a60001ec30e605c500776c226d8e0
AliNiestani <aliniestani@gmail.com>
2016-05-12 14:52:14 +0430

    Update lock_forward.lua

--- plugins/lock_forward.lua
+++ plugins/lock_forward.lua
@@ -2,10 +2,11 @@ do
 
 local function pre_process(msg)
     
+    --Checking mute
     local hash = 'mate:'..msg.to.id
-    if redis:get(hash) and msg.fwd_from and not is_momod(msg)  then
-    chat_del_user('chat#id'..msg.to.id,'user#id'..msg.from.id, ok_cb, false)
-            return "Forward was locked"
+    if redis:get(hash) and msg.fwd_from and not is_sudo(msg) and not is_owner(msg) and not is_momod(msg) and not is_admin1(msg)  then
+            delete_msg(msg.id, ok_cb, true)
+            return "done"
         end
     
         return msg
@@ -15,34 +16,26 @@ local function pre_process(msg)
 
 
 local function run(msg, matches)
-    chat_id = msg.to.id
+    chat_id =  msg.to.id
     
-    if matches[1] == 'lock' and is_momod(msg) then
+    if is_momod(msg) and matches[1] == 'lock' then
       
             
                     local hash = 'mate:'..msg.to.id
                     redis:set(hash, true)
                     return "قفل فروارد فعال شد"
-  elseif matches[1] == 'unlock' and is_momod(msg) then
+  elseif is_momod(msg) and matches[1] == 'unlock' then
                     local hash = 'mate:'..msg.to.id
                     redis:del(hash)
                     return "قفل فروارد غیر فعال شد"
-          end
-          if matches[1] == 'status' then
-                    local hash = 'mate:'..msg.to.id
-                    if redis:get(hash) then
-                    return "Forward is locked"
-          else 
-          return "Forward is not locked"
+end
 
 end
-end
-end
+
 return {
     patterns = {
-        '[#!/](lock) [Ff]orward$',
-        '[#!/](unlock) [Ff]orward$',
-    '^[#!/][Ff]orward ([Ss]tatus)$',
+        '^[#!/](lock) [Ff]orward$',
+        '^[#!/](unlock) [Ff]orward$'
     },
     run = run,
     pre_process = pre_process
