a9ffda496ea39f94b8a7ce56392c81796e31cb77
AliNiestani <aliniestani@gmail.com>
2016-05-22 12:47:21 +0430

    Update tts.lua

--- plugins/tts.lua
+++ plugins/tts.lua
@@ -4,8 +4,8 @@ local function run(msg, matches)
   local eq = URL.escape(matches[1])
 
   local url = "http://translate.google.com/translate_tts?ie=UTF-8&total=1&idx=0&textlen=32&client=tw-ob&q="..eq.."&tl=En-gb"
-  local receiver = get_receiver(msg)
-  send_audio_from_url(receiver, url, send_title, {receiver, title})
+  local file = download_to_file(url,'text.ogg')
+      send_audio('channel#id'..msg.to.id, file, ok_cb , false)
 end
 
 return {
